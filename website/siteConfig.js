9330df6796bcc40237bd1eebadb602fe8d1e27e2
Bob Hageman <bob@schluss.org>
2023-11-16 11:10:54 +0100

    correct baseurl

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'IRMA docs', // Title for your website.
   tagline: 'Technical documentation of the IRMA project',
   url: 'https://irma.app', // Your website URL
-  baseUrl: '/', // Base URL for your project
+  baseUrl: '/irma-documentation/', // Base URL for your project
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
